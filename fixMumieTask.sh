859014212da9b0ff2c00da8497c66602041ab61c
tobias <tobias.goltz@integral-learning.de>
2023-01-23 17:02:56 +0100

    added scripts to install and use php-cs-fixer

---
+++ fixMumieTask.sh
@@ -0,0 +1,2 @@
+cd ${ILIAS_LOCAL_ROOT}
+tools/php-cs-fixer/vendor/bin/php-cs-fixer fix Customizing/global/plugins/Services/Repository/RepositoryObject/MumieTask/
---
+++ installPhpCsFixer.sh
@@ -0,0 +1,3 @@
+cd ${ILIAS_LOCAL_ROOT}
+mkdir -p tools/php-cs-fixer
+composer require --working-dir=tools/php-cs-fixer friendsofphp/php-cs-fixer
